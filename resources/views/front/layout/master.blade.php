f9bd7baf78afbe72bb4f4d182256450c0dbdebea
SujoySust <sujoy.cse50@gmail.com>
2019-11-15 16:04:28 +0600

    first commit

---
+++ resources/views/front/layout/master.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+@include('front.layout.top')
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar">
+<div id="main-wrapper">
+    <!-- Page Preloader -->
+    <div id="preloader">
+        <div id="status">
+            <div class="status-mes"></div>
+        </div>
+    </div>
+    <!-- preloader -->
+
+    <div class="uc-mobile-menu-pusher">
+        <div class="content-wrapper">
+            @include('front.layout.header')
+
+            <!-- header_section_wrapper -->
+                @yield('content')
+
+
+            <!-- Subscriber Section -->
+        </div>
+        <!-- #content-wrapper -->
+
+    </div>
+    <!-- .offcanvas-pusher -->
+
+    <a href="#" class="crunchify-top"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
+
+    <div class="uc-mobile-menu uc-mobile-menu-effect">
+        <button type="button" class="close" aria-hidden="true" data-toggle="offcanvas"
+                id="uc-mobile-menu-close-btn">&times;</button>
+        <div>
+            <div>
+                <ul id="menu">
+                    <li class="active"><a href="blog.html">News</a></li>
+                    <li><a href="category.html">Mobile</a></li>
+                    <li><a href="blog.html">Tablet</a></li>
+                    <li><a href="category.html">Gadgets</a></li>
+                    <li><a href="blog.html">Camera</a></li>
+                    <li><a href="category.html">Design</a></li>
+                    <li class="dropdown m-menu-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle">More
+                            <span><i class="fa fa-angle-down"></i></span></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <div class="m-menu-content">
+                                    <ul class="col-sm-3">
+                                        <li class="dropdown-header">Widget Haeder</li>
+                                        <li><a href="#">Awesome Features</a></li>
+                                        <li><a href="#">Clean Interface</a></li>
+                                        <li><a href="#">Available Possibilities</a></li>
+                                        <li><a href="#">Responsive Design</a></li>
+                                        <li><a href="#">Pixel Perfect Graphics</a></li>
+                                    </ul>
+                                    <ul class="col-sm-3">
+                                        <li class="dropdown-header">Widget Haeder</li>
+                                        <li><a href="#">Awesome Features</a></li>
+                                        <li><a href="#">Clean Interface</a></li>
+                                        <li><a href="#">Available Possibilities</a></li>
+                                        <li><a href="#">Responsive Design</a></li>
+                                        <li><a href="#">Pixel Perfect Graphics</a></li>
+                                    </ul>
+                                    <ul class="col-sm-3">
+                                        <li class="dropdown-header">Widget Haeder</li>
+                                        <li><a href="#">Awesome Features</a></li>
+                                        <li><a href="#">Clean Interface</a></li>
+                                        <li><a href="#">Available Possibilities</a></li>
+                                        <li><a href="#">Responsive Design</a></li>
+                                        <li><a href="#">Pixel Perfect Graphics</a></li>
+                                    </ul>
+                                    <ul class="col-sm-3">
+                                        <li class="dropdown-header">Widget Haeder</li>
+                                        <li><a href="#">Awesome Features</a></li>
+                                        <li><a href="#">Clean Interface</a></li>
+                                        <li><a href="#">Available Possibilities</a></li>
+                                        <li><a href="#">Responsive Design</a></li>
+                                        <li><a href="#">Pixel Perfect Graphics</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- .uc-mobile-menu -->
+
+</div>
+<!-- #main-wrapper -->
+
+<!-- jquery Core-->
+<script src="{{ asset('front/js/jquery-2.1.4.min.js')}}"></script>
+
+<!-- Bootstrap -->
+<script src="{{ asset('front/js/bootstrap.min.js')}}"></script>
+
+<!-- Theme Menu -->
+<script src="{{ asset('front/js/mobile-menu.js')}}"></script>
+
+<!-- Owl carousel -->
+<script src="{{ asset('front/js/owl.carousel.min.js')}}"></script>
+
+<!-- Theme Script -->
+<script src="{{ asset('front/js/script.js')}}"></script>
+</body>
+</html>
